26466d58e6542275f87eab08e24ad7c81136dea0
Indulgent丶徴 <1343837706@qq.com>
2019-02-10 19:47:47 +0800

    修改了BUG

--- function.py
+++ function.py
@@ -24,30 +24,30 @@ def rename_file(path='.', prefix='', num=None):
         name = part_index[num]
         for d in os.listdir(path):
             if name in d:
-                newname = re.search('\d+\.\s?' + name + '\..*', a).group()
+                newname = re.search('\d+\.\s*' + name + '\..*', d).group()
                 os.rename(d, newname)
                 return
-
+    print('prefix', prefix)
     for d in os.listdir(path):
         if d.endswith('xml'):
             os.remove(os.path.join(directory, d))
         elif d.endswith('download'):
             pass
         else:
-
-            temp = re.search('(' + prefix + ')?' + r'[\s#]?(\d+).*(\..*)', d)
+            temp = re.search('(' + prefix + ')*' + r'[\s#]*?(\d+).*(\..*)', d)
             if temp:
                 try:
                     newname = temp.group(2) + '.' + part_index[int(temp.group(2))] + temp.group(3)
                     if d != newname:
                         os.rename(d, newname)
                         print('重命名：', d, '-->', newname)
-                except KeyError:
+                except KeyError as e:
                     print('重命名失败，请检查已下载文件前缀是否与视频标题一致，\
-                    若不一致，请在main 方法里手动指定prefix')
+                    若不一致，请在main 方法里手动指定prefix\n',e)
+                    raise KeyError
                     return
             else:
-                newname = re.sub('(' + prefix + ')?' + r'[\s#]?', '', d)
+                newname = re.sub('(' + prefix + ')?' + r'[\s#]*', '', d)
                 if d != newname:
                     print('重命名：', d, '-->', newname)
                     os.rename(d, newname)
@@ -64,7 +64,7 @@ def get_index(url):
     # 找到数据位置
     pattern = '<script>.*?({[^<]*}).*?</script>'
     # 清洗多余js
-    fun_pattern = r';?\s?\(function\(.*?\{.*\}$'
+    fun_pattern = r';*?\s*?\(function\(.*?\{.*\}$'
     # 获取标题
     title_pattern = '<h1 title="([^"]+?)"'
     header = {
